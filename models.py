e77795d8e1ad15b68daa953f0337ed4808348ae6
Jean Schindler <jeanlucaschindler@gmail.com>
2025-02-09 12:54:35 +0000

    cleaned and organized workspace

---
+++ models.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import os
+from google import genai
+from openai import OpenAI
+
+# Load environment variables
+load_dotenv()
+deepseek_api = os.getenv('DEEPSEEK_API_KEY')
+gemini_api = os.getenv('GEMINI_API_KEY')
+
+clientgpt = OpenAI(
+    api_key=os.environ.get("FUSE_OPEN_AI_KEY"))
+
+clientgem = genai.Client(api_key=gemini_api)
+
+
+
+def get_theme_allocation_gemini(page_text, system_instructions, model="gemini-2.0-flash"):
+    try:
+        response = clientgem.models.generate_content(
+            model=model, contents=f"{system_instructions}. Text: {page_text}"
+        )
+        return response.text.strip()
+
+    except Exception as e:
+        print(f"Error processing page: {str(e)}")
+
+def get_theme_allocation_openai(page_text, system_instructions, model):
+    try:
+        response = clientgpt.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": system_instructions},
+                {"role": "user", "content": page_text}
+            ],
+            max_tokens=100,
+            temperature=0.0
+        )
+        allocation = response.choices[0].message.content.strip()
+        return allocation
+
+    except Exception as e:
+        print(f"Error processing page: {str(e)}")
