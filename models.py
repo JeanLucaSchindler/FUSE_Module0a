90b12bc76c38593559919a7bf04e89edc254d992
Jean Schindler <jeanlucaschindler@gmail.com>
2025-03-14 20:25:39 +0000

    adding file updates

--- models.py
+++ models.py
@@ -8,12 +8,23 @@ load_dotenv()
 deepseek_api = os.getenv('DEEPSEEK_API_KEY')
 gemini_api = os.getenv('GEMINI_API_KEY')
 
-clientgpt = OpenAI(
-    api_key=os.environ.get("FUSE_OPEN_AI_KEY"))
-
+clientgpt = OpenAI(api_key=os.environ.get("FUSE_OPEN_AI_KEY"))
 clientgem = genai.Client(api_key=gemini_api)
+client_deepseek = OpenAI(api_key=deepseek_api, base_url="https://api.deepseek.com")
 
 
+def get_theme_allocation_deepseek(page_text, system_instructions, model="deepseek-reasoner"):
+    try:
+        response = client_deepseek.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system_instructions},
+            {"role": "user", "content": page_text},],stream=False)
+
+        return response.choices[0].message.content.strip()
+
+    except Exception as e:
+        print(f"Error processing page: {str(e)}")
 
 def get_theme_allocation_gemini(page_text, system_instructions, model="gemini-2.0-flash"):
     try:
